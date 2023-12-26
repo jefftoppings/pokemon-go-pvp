86e89f3d14de87e010dbfcdd54d59a5cb933b2bb
Jeff Toppings <jeffrey.toppings@outlook.com>
2023-12-26 16:17:19 -0600

    fix: models to be more in line with json, omit some fields

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,20 +1,39 @@
 package model
 
-type PokedexEntry struct {
-	ID            string `json:"id"`
-	FormID        string `json:"form_id"`
-	PokedexNumber int64  `json:"pokedex_number"`
-	Generation    int64  `json:"generation"`
-	Name          string `json:"name"`
-	Stats         struct {
-		Attack  int64 `json:"attack"`
-		Defense int64 `json:"defense"`
-		Stamina int64 `json:"stamina"`
-	} `json:"stats"`
-	PrimaryType   string `json:"primary_type"`
-	SecondaryType string `json:"secondary_type"`
-	ImageUrl      string `json:"image_url"`
-	Evolutions    []struct {
-		ID string `json:"id"`
-	} `json:"evolutions"`
+type Pokemon struct {
+	ID                  string                   `json:"id"`
+	FormID              string                   `json:"formId"`
+	DexNr               int                      `json:"dexNr"`
+	Generation          int                      `json:"generation"`
+	Names               NameTranslations         `json:"names"`
+	Stats               PokemonStats             `json:"stats"`
+	PrimaryType         PokemonType              `json:"primaryType"`
+	SecondaryType       PokemonType              `json:"secondaryType"`
+	Assets              PokemonAssets            `json:"assets"`
+	Evolutions          []Evolution              `json:"evolutions"`
+}
+
+type NameTranslations struct {
+	English  string `json:"English"`
+}
+
+type PokemonStats struct {
+	Stamina int `json:"stamina"`
+	Attack  int `json:"attack"`
+	Defense int `json:"defense"`
+}
+
+type PokemonType struct {
+	Type  string           `json:"type"`
+	Names NameTranslations `json:"names"`
+}
+
+type PokemonAssets struct {
+	Image      string `json:"image"`
+	ShinyImage string `json:"shinyImage"`
+}
+
+type Evolution struct {
+	ID      string        `json:"id"`
+	FormID  string        `json:"formId"`
 }
