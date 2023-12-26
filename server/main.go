a8be617ce55ecac21e4364f34d72acef966ed0ff
Jeff Toppings <jeffrey.toppings@outlook.com>
2023-12-26 16:47:07 -0600

    feat: add apiKey as auth, gitignore config

--- server/main.go
+++ server/main.go
@@ -1,21 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
-    "github.com/jefftoppings/pokemon-go-pvp/internal/api"
+	"github.com/gorilla/mux"
+
+	"github.com/jefftoppings/pokemon-go-pvp/internal/api"
 )
 
+type Config struct {
+	APIKey string `json:"api_key"`
+}
+
+var config Config
+
 func main() {
-    r := mux.NewRouter()
+	err := loadConfig("config.json")
+	if err != nil {
+		fmt.Println("Error loading configuration:", err)
+		return
+	}
 
-    // Routes
-	r.HandleFunc("/api/search-pokemon", api.SearchPokemon).Methods("GET")
+	r := mux.NewRouter()
 
-    // Start the HTTP server
-    http.Handle("/", r)
-    fmt.Println("Server is running on :8080")
-    http.ListenAndServe(":8080", nil)
+	// Routes
+	r.HandleFunc("/api/search-pokemon", AuthMiddleware(api.SearchPokemon)).Methods("GET")
+
+	// Start the HTTP server
+	http.Handle("/", r)
+	fmt.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+// AuthMiddleware is a middleware function that checks for a valid API key in the request headers
+func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKeyFromRequest := r.Header.Get("X-API-Key")
+
+		if apiKeyFromRequest != config.APIKey {
+			http.Error(w, "Unauthorized. You need a valid api key to call this.", http.StatusUnauthorized)
+			return
+		}
+
+		// Call the next handler if authentication is successful
+		next.ServeHTTP(w, r)
+	}
+}
+
+func loadConfig(filePath string) error {
+	fileContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to read configuration file: %v", err)
+	}
+
+	if err := json.Unmarshal(fileContent, &config); err != nil {
+		return fmt.Errorf("failed to unmarshal configuration data: %v", err)
+	}
+
+	return nil
 }
